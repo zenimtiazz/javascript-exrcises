8bb94e7363fa96234f124a375df070faa7c0632a
Zain Imtiaz <zenimtiazz@gmail.com>
2021-09-29 12:03:46 +0200

    answers added

--- 04-maths/06-factorial-Optional/script.js
+++ 04-maths/06-factorial-Optional/script.js
@@ -1,12 +1,14 @@
 (function() {
+
+document.getElementById("run").addEventListener("click", function() { // button functionality
+    let num =document.getElementById("number").value //let is used to declare variable in javascript
+    if (num == 0 || num == 1) // compare values
+        num= 1;
     
-    // to get the value of an input: document.getElementById("element-id").value
-
-    document.getElementById("run").addEventListener("click", function() {
-
-        const numberField = document.getElementById("number");
-        const newValue = numberField.value = Math.pow(numberField.value, 2);
-
-    });
-
-})();
+    for (var i = num - 1; i >= 1; i--) {
+              num *= i;
+        }
+        
+    document.getElementById("res").innerHTML=num;
+         } )
+        })();
